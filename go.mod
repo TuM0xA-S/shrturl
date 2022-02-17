fb852ce023d54f069217dffd1ab2f193af3138d2
Timofey Safronov <tsafronov@ozon.ru>
2022-02-17 15:19:31 +0300

    add env parse

--- go.mod
+++ go.mod
@@ -5,10 +5,12 @@ go 1.17
 require github.com/labstack/echo/v4 v4.6.3
 
 require (
+	github.com/caarlos0/env/v6 v6.9.1 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/go-redis/redis/v8 v8.11.4 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/labstack/gommon v0.3.1 // indirect
 	github.com/mattn/go-colorable v0.1.11 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
--- main.go
+++ main.go
@@ -5,25 +5,39 @@ import (
 	"log"
 	"time"
 
+	"github.com/caarlos0/env/v6"
 	"github.com/go-redis/redis/v8"
+	_ "github.com/joho/godotenv/autoload"
 	"github.com/labstack/echo/v4/middleware"
 )
 
+type config struct {
+	RedisAddr     string        `env:"REDIS_ADDR"`
+	RedisUser     string        `env:"REDIS_USER"`
+	RedisPassword string        `env:"REDIS_PASSWORD"`
+	RedisDB       int           `env:"REDIS_DB"`
+	ExpiryTime    time.Duration `env:"EXPIRY_TIME"`
+}
+
 func main() {
+	var cfg config
+	if err := env.Parse(&cfg); err != nil {
+		log.Fatal(err)
+	}
 	ctx := context.Background()
 
 	db := redis.NewClient(&redis.Options{
-		Addr:     "",
-		Username: "",
-		Password: "",
-		DB:       0,
+		Addr:     cfg.RedisAddr,
+		Username: cfg.RedisUser,
+		Password: cfg.RedisPassword,
+		DB:       cfg.RedisDB,
 	})
 
 	if err := db.Ping(ctx).Err(); err != nil {
 		log.Fatal(err)
 	}
 
-	shrt := NewShrt(ctx, ShrtCfg{DB: db, ExpirationTime: 10 * time.Second})
+	shrt := NewShrt(ctx, ShrtCfg{DB: db, ExpirationTime: cfg.ExpiryTime})
 	shrt.Use(middleware.Logger(), middleware.Recover())
 
 	// Start server
